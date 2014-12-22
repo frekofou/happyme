6af70eb15e9973aa6e6a78c1c8e777c32c110308
frekofou <frederic.marianne@gmail.com>
2014-12-22 16:22:33 +0100

    commit de reparation

--- src/main/java/happyme/HappyService.java
+++ src/main/java/happyme/HappyService.java
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-package happyme;
-
-import net.vz.mongodb.jackson.JacksonDBCollection;
-
-import com.mongodb.DB;
-import com.mongodb.Mongo;
-import com.yammer.dropwizard.Service;
-import com.yammer.dropwizard.config.Bootstrap;
-import com.yammer.dropwizard.config.Environment;
- 
-public class HappyService extends Service<HappyConfiguration> {
- 
-    public static void main(String[] args) throws Exception {
-        new HappyService().run(new String[] { "server" });
-    }
- 
-    @Override
-    public void initialize(Bootstrap<HappyConfiguration> bootstrap) {
-        bootstrap.setName("happyme");
-    }
- 
-    @Override
-    public void run(HappyConfiguration configuration, Environment environment) throws Exception {
-
-    	Mongo mongo = new Mongo(configuration.mongohost, configuration.mongoport);
-        HappyMongoManaged mongoManaged = new HappyMongoManaged(mongo);
-        environment.manage(mongoManaged);
- 
-        environment.addHealthCheck(new HappyMongoHealthCheck(mongo));
- 
-
-        DB db = mongo.getDB(configuration.mongodb);
-        JacksonDBCollection<HappymeEmotion, String> emotions = JacksonDBCollection.wrap(db.getCollection("emotions"), HappymeEmotion.class, String.class);
- 
-        environment.addResource(new IndexResource(emotions));
- 
-        environment.addResource(new HappyRessourceAction(emotions));
-    
-   
-    }
- 
-=======
 package happyme;
 
 import net.vz.mongodb.jackson.JacksonDBCollection;
@@ -82,5 +39,5 @@ public class HappyService extends Service<HappyConfiguration> {
    
     }
  
->>>>>>> branch 'home' of https://github.com/frekofou/happyme.git
+
 }
