c949fe404a3aefd9770ee9e8abbbfa08fe739da6
frekofou <frederic.marianne@gmail.com>
2015-04-07 00:25:08 +0200

    fix emotions, fix put, fix type

--- src/main/java/happyme/model/HappymeEmotionType.java
+++ src/main/java/happyme/model/HappymeEmotionType.java
@@ -12,29 +12,46 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @Table(name = "feeling")
 public class HappymeEmotionType {
 
-	@Column(name = "feeling", nullable = false, updatable = true)
-    @Enumerated(EnumType.STRING)
+	// @Column(name = "feeling", nullable = true, updatable = true)
+	// @Enumerated(EnumType.STRING)
 	private EmotionType type;
-	public EmotionType getType() {return type;}
-	public void setType(EmotionType type) {this.type = type;}
-
-    @JsonFormat(shape = JsonFormat.Shape.OBJECT)
+ 
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	public enum EmotionType {
 		// possible emotions
-		ANGRY(5, "ANGRY"), LOVE(15,"LOVE"), HAPPY(10,"HAPPY"), SAD(7, "SAD");
-		// this how the algo take the differences between the different emotions
-		private String feel = ""; 
+		ANGRY(5, "ANGRY"), LOVE(15, "LOVE"), HAPPY(10, "HAPPY"), SAD(7, "SAD");
+		// this how the algo get the differences between the different emotions
+		private String feeling;
 		private int weight;
 
-		private EmotionType(int w, String f) {
-			feel = f;
-			weight = w;
+		public String getFeeling() {
+			return feeling;
+		}
+
+		public void setFeeling(String feeling) {
+			this.feeling = feeling;
 		}
 
 		
-		public String getFeel() {
-			return feel;
+
+		public int getWeight() {
+			return weight;
 		}
 
+		private EmotionType(int w, String f) {
+			feeling = f;
+			weight = w;
+		}
+		
+		
+
+	}
+	
+	public EmotionType getType() {
+		return type;
+	}
+
+	public void setType(EmotionType type) {
+		this.type = type;
 	}
 }
